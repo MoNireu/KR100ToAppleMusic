b0bc639bcbdd19a3618063b6d63bf2a6f4a00d24
MoNireu <monireu.dev@gmail.com>
2020-01-02 12:12:27 +0900

    feat: Create parsing and crawling logic

---
+++ KR100ToAppleMusic/Model/MusicChartListModel.swift
@@ -0,0 +1,69 @@
+//
+//  MusicChartList.swift
+//  KR100ToAppleMusic
+//
+//  Created by MoNireu on 2020/01/02.
+//  Copyright © 2020 monireu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftSoup
+
+class MusicChartListModel {
+    
+    func parseResult() -> String {
+        var result: String?
+        let url = "https://m.app.melon.com/cds/main/mobile4web/main_chartPaging.htm"
+        let userAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+        let header : HTTPHeaders = ["User-Agent" : "\(userAgentString)"]
+        let param = [
+            "startIndex" : 1,
+            "pageSize" : 50,
+            "rowsCnt" : 100
+        ]
+        
+        let req = AF.request(url, parameters: param, headers: header)
+        
+        let resultHTML = req.responseString() { res in
+            result = res.value
+        }
+        return result!
+    }
+    
+    func parse (html: String?, success: (()->Void)? = nil, fail: (()->Void)? = nil) {
+        self.activityIndicator.startAnimating()
+        do {
+            let doc: Document = try SwiftSoup.parseBodyFragment(html!)
+            let artist = try doc.getElementsByClass("name ellipsis")
+            let title = try doc.getElementsByClass("title ellipsis")
+            for i in 0 ..< artist.count {
+                let musicInfo = MusicInfoVO()
+                musicInfo.rank   = String(i + 1)
+                musicInfo.music  = try title.get(i).text()
+                musicInfo.artist = try artist.get(i).text()
+                
+                self.musicChartList.append(musicInfo)
+            }
+            
+            for vo in self.musicChartList {
+                print("\(vo.rank!) / \(vo.artist!) / \(vo.music!)")
+            }
+            
+        } catch Exception.Error(let type, let message) {
+            print("")
+        } catch {
+            print("")
+        }
+        
+        success?()
+    }
+    
+    
+//    self.parse(html: html,
+//               success: {
+//                self.performSegue(withIdentifier: "toMusicChartList", sender: self)
+//                self.activityIndicator.stopAnimating()
+//    })
+    
+}
---
+++ KR100ToAppleMusic/Model/MusicInfoVO.swift
@@ -0,0 +1,16 @@
+//
+//  MusicInfoVO.swift
+//  KR100ToAppleMusic
+//
+//  Created by MoNireu on 2020/01/02.
+//  Copyright © 2020 monireu. All rights reserved.
+//
+
+import Foundation
+
+
+class MusicInfoVO {
+    var rank: String?
+    var music: String?
+    var artist: String?
+}
