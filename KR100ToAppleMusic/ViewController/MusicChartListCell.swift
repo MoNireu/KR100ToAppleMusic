be6a9051dda01cc2d0095b2df0cf066c27dcb205
MoNireu <monireu.dev@gmail.com>
2020-01-02 11:52:08 +0900

    feat: Connect custom class to MusicChartListVC and MusicChartListCell

---
+++ KR100ToAppleMusic/ViewController/MusicChartListCell.swift
@@ -0,0 +1,29 @@
+//
+//  MusicChartListCell.swift
+//  KR100ToAppleMusic
+//
+//  Created by MoNireu on 2020/01/02.
+//  Copyright © 2020 monireu. All rights reserved.
+//
+
+import UIKit
+
+class MusicChartListCell: UITableViewCell {
+
+    @IBOutlet var rank: UILabel!
+    @IBOutlet var music: UILabel!
+    @IBOutlet var artist: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
