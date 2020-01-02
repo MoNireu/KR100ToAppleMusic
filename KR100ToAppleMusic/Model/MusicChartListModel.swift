174f68fd937b653512554f9567f3459cc0ab3d51
MoNireu <monireu.dev@gmail.com>
2020-01-02 13:54:49 +0900

    fix: Fix Parsing and Crawling error

--- KR100ToAppleMusic/Model/MusicChartListModel.swift
+++ KR100ToAppleMusic/Model/MusicChartListModel.swift
@@ -14,7 +14,7 @@ class MusicChartListModel {
     
     var musicChartList = [MusicInfoVO]()
     
-    func parseResult() -> String {
+    func parseResult(success: (()->Void)? = nil, fail: (()->Void)? = nil){
         var result: String?
         let url = "https://m.app.melon.com/cds/main/mobile4web/main_chartPaging.htm"
         let userAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
@@ -28,38 +28,35 @@ class MusicChartListModel {
         let req = AF.request(url, parameters: param, headers: header)
         
         let resultHTML = req.responseString() { res in
-            result = res.value
-        }
-        return result!
-    }
-    
-    func parse (html: String?, success: (()->Void)? = nil, fail: (()->Void)? = nil) {
-        do {
-            let doc: Document = try SwiftSoup.parseBodyFragment(html!)
-            let artist = try doc.getElementsByClass("name ellipsis")
-            let title = try doc.getElementsByClass("title ellipsis")
-            for i in 0 ..< artist.count {
-                let musicInfo = MusicInfoVO()
-                musicInfo.rank   = String(i + 1)
-                musicInfo.music  = try title.get(i).text()
-                musicInfo.artist = try artist.get(i).text()
+            let html = res.value
+            
+            do {
+                let doc: Document = try SwiftSoup.parseBodyFragment(html!)
+                let artist = try doc.getElementsByClass("name ellipsis")
+                let title = try doc.getElementsByClass("title ellipsis")
+                for i in 0 ..< artist.count {
+                    let musicInfo = MusicInfoVO()
+                    musicInfo.rank   = String(i + 1)
+                    musicInfo.music  = try title.get(i).text()
+                    musicInfo.artist = try artist.get(i).text()
+                    
+                    self.musicChartList.append(musicInfo)
+                }
                 
-                self.musicChartList.append(musicInfo)
+                for vo in self.musicChartList {
+                    print("\(vo.rank!) / \(vo.artist!) / \(vo.music!)")
+                }
+                
+            } catch Exception.Error(let type, let message) {
+                print("\(type) / \(message)")
+            } catch {
+                print("에러가 발생했습니다.")
             }
             
-            for vo in self.musicChartList {
-                print("\(vo.rank!) / \(vo.artist!) / \(vo.music!)")
-            }
+            success?()
             
-        } catch Exception.Error(let type, let message) {
-            print("\(type) / \(message)")
-        } catch {
-            print("에러가 발생했습니다.")
         }
-        
-        success?()
     }
-    
     
 //    self.parse(html: html,
 //               success: {
--- KR100ToAppleMusic/ViewController/MusicChartListVC.swift
+++ KR100ToAppleMusic/ViewController/MusicChartListVC.swift
@@ -15,33 +15,30 @@ class MusicChartListVC: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.parsedHTML = model.parseResult()
-        model.parse(html: self.parsedHTML,
-                                  success: {
-                                    self.alert("불러오기를 성공했습니다.")},
-                                  fail: nil)
+        model.parseResult(success: {
+            self.alert("불러오기를 성공했습니다.")
+            self.tableView.reloadData()
+        }, fail: nil)
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return model.musicChartList.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? MusicChartListCell else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "chart_cell", for: indexPath) as? MusicChartListCell else {
+                print("error")
                 return UITableViewCell()
+                
             }
             
 
         cell.rank.text   = (self.model.musicChartList[indexPath.row].rank)! + "위"
+        cell.rank.sizeToFit()
         cell.music.text  = self.model.musicChartList[indexPath.row].music
         cell.artist.text = self.model.musicChartList[indexPath.row].artist
             
